0ac3e85ed377bb436253a71e654f62ec90d1bc00
6u5t4v <41763143+6u5t4v@users.noreply.github.com>
2019-11-22 17:54:26 +0100

    First setup
    Removed
    - Stringlenght.java
    - Commented ChatFormat onChat method: It was returning an unknown error...

--- Core/src/com/Furnesse/core/listeners/ChatFormat.java
+++ Core/src/com/Furnesse/core/listeners/ChatFormat.java
@@ -1,37 +1,28 @@
 package com.Furnesse.core.listeners;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-
-import com.Furnesse.core.Core;
-import com.Furnesse.core.utils.Lang;
-
-import me.clip.placeholderapi.PlaceholderAPI;
 
 public class ChatFormat implements Listener {
 
-	Core plugin = Core.instance;
-
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onChat(AsyncPlayerChatEvent e) {
-		if (plugin.chatFormats == null || plugin.chatFormats.isEmpty()) {
-			return;
-		}
-
-		Player player = e.getPlayer();
-//		Rank rank = plugin.getRanks().getPlayerRank(player);
-		String msg = e.getMessage();
-
-		for (String cFormat : plugin.chatFormats) {
-			String format = plugin.getConfig().getString("chat.formats." + cFormat + ".format");
-			if (player.hasPermission("core.chat." + cFormat) || cFormat.equals("default")) {
-				format = PlaceholderAPI.setPlaceholders(player, format);
-				e.setFormat(Lang.chat(format).replace("%msg%", msg));
-				e.setMessage(e.getFormat());
-			}
-		}
-	}
+//	Core plugin = Core.instance;
+//
+//	@EventHandler(priority = EventPriority.HIGHEST)
+//	public void onChat(AsyncPlayerChatEvent e) {
+//		if (plugin.chatFormats == null || plugin.chatFormats.isEmpty()) {
+//			return;
+//		}
+//
+//		Player player = e.getPlayer();
+////		Rank rank = plugin.getRanks().getPlayerRank(player);
+//		String msg = e.getMessage();
+//
+//		for (String cFormat : plugin.chatFormats) {
+//			String format = plugin.getConfig().getString("chat.formats." + cFormat + ".format");
+//			if (player.hasPermission("core.chat." + cFormat) || cFormat.equals("default")) {
+//				format = PlaceholderAPI.setPlaceholders(player, format);
+//				e.setFormat(Lang.chat(format).replace("%msg%", msg));
+//				e.setMessage(e.getFormat());
+//			}
+//		}
+//	}
 }
